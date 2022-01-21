fc9e8ada51e2566ca6830f0f7cd2b1b80e6990f5
masterSal <salimabdellanuru@gmail.com>
2022-01-21 12:28:08 +0300

    fix a bug on regex where the from must include '+' and digit from 0-1

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:)[0-1A-Za-z]+|(?<=to:)[0-9+]+|(?<=flags:)[-:0-1]+/).join(',')
+puts ARGV[0].scan(/(?<=from:)[0-9A-Za-z+]+|(?<=to:)[0-9+]+|(?<=flags:)[-:0-1]+/).join(',')
